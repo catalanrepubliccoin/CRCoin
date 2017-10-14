1e2ff619f7f3da7fe36551b7aca344f56e6bcbc9
catalanrepubliccoin <32789846+catalanrepubliccoin@users.noreply.github.com>
2017-10-14 14:34:56 +0200

    First commit

---
+++ tests/CMakeFiles/SystemTests.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/nova/CRCoin/tests/System/ContextGroupTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/ContextGroupTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/ContextGroupTimeoutTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/ContextGroupTimeoutTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/ContextTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/ContextTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/DispatcherTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/DispatcherTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/ErrorMessageTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/ErrorMessageTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/EventLockTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/EventLockTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/EventTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/EventTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/Ipv4AddressTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/Ipv4AddressTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/Ipv4ResolverTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/Ipv4ResolverTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/OperationTimeoutTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/OperationTimeoutTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/RemoteContextTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/RemoteContextTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/TcpConnectionTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/TcpConnectionTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/TcpConnectorTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/TcpConnectorTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/TcpListenerTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/TcpListenerTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/TimerTests.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/TimerTests.cpp.o"
+  "/home/ubuntu/nova/CRCoin/tests/System/main.cpp" "/home/ubuntu/nova/CRCoin/build/release/tests/CMakeFiles/SystemTests.dir/System/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../external/gtest/include"
+  "../../external/gtest"
+  "../../tests/../version"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/nova/CRCoin/build/release/src/CMakeFiles/System.dir/DependInfo.cmake"
+  "/home/ubuntu/nova/CRCoin/build/release/external/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/ubuntu/nova/CRCoin/build/release/external/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
